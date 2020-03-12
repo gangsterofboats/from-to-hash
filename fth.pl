95c46d4eca6830fef67546c097f326171bf2b7e2
Michael Wiseman <36470882+gangsterofboats@users.noreply.github.com>
2020-03-11 17:56:11 -0700

    Update fth.pl

--- fth.pl
+++ fth.pl
@@ -1,3 +1,22 @@
+#!/usr/bin/env perl
+#############################################################################
+# FromToHash - Solve a job interview puzzle                                 #
+# Copyright (C) 2020 Michael Wiseman                                        #
+#                                                                           #
+# This program is free software: you can redistribute it and/or modify      #
+# it under the terms of the GNU Affero General Public License as            #
+# published by the Free Software Foundation, either version 3 of the        #
+# License, or (at your option) any later version.                           #
+#                                                                           #
+# This program is distributed in the hope that it will be useful,           #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+# GNU Affero General Public License for more details.                       #
+#                                                                           #
+# You should have received a copy of the GNU Affero General Public License  #
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.    #
+#############################################################################
+
 use strict;
 use warnings;
 use feature 'say';
@@ -36,5 +55,5 @@ sub from_hash
     return scalar reverse $output;
 }
 
-# say to_hash('leepadg');
+say to_hash('leepadg');
 say from_hash('910897038977002', 9);
